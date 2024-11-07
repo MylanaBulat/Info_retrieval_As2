cbcf6880e406c22ce621a5b80129e1c732ac3db5
Mylana Bulat <mylanabulat@gmail.com>
2024-11-07 11:31:39 +0000

    2. Parsed and indexed documents from the Latimes documents folder

--- src/Main.java
+++ src/Main.java
@@ -23,57 +23,83 @@ import org.jsoup.select.Elements;
 /**
  * This is a java Doc explanation of the code below
  * 1. Mapping each SGML tag <DOCNO>, <TEXT>, etc., into corresponding Lucene document fields.
+ * Method parseFBISDocument - processes each singular document from a file in a fbis folder
+ * Method parseLADocument - processes each singular document from a file in a latimes folder
+ * Method rocessFbisLatimesFolder runs the processing for all
  */
 
 public class Main {
 
     private static String INDEX_DIRECTORY = System.getProperty("user.dir") + File.separator + "index";
     private static String FILE_PATH = System.getProperty("user.dir") + File.separator + "documents";
-    private static String FILE_PATH_FBIS = System.getProperty("user.dir") + File.separator + "documents" + File.separator + "fbis";
+    private static String FILE_PATH_FBIS = FILE_PATH + File.separator + "fbis";
+    private static String FILE_PATH_LATIMES = FILE_PATH + File.separator + "latimes";
 
     // Method to parse a single FBIS file and return a Lucene document
     public static Document parseFBISDocument(Element docElement) {
         Document luceneDoc = new Document();
 
-        // Extract <DOCNO>
         Element docnoElement = docElement.selectFirst("DOCNO");
         if (docnoElement != null) {
             luceneDoc.add(new StringField("docno", docnoElement.text(), Field.Store.YES));
         }
 
-        // Extract <HEADER>
         Element headerElement = docElement.selectFirst("HEADER");
         if (headerElement != null) {
             luceneDoc.add(new TextField("header", headerElement.text(), Field.Store.YES));
         }
 
-        // Extract <TEXT>
         Element textElement = docElement.selectFirst("TEXT");
         if (textElement != null) {
             luceneDoc.add(new TextField("text", textElement.text(), Field.Store.YES));
         }
 
-        // Extract other fields such as <DATE1>
         Element dateElement = docElement.selectFirst("DATE1");
         if (dateElement != null) {
             luceneDoc.add(new StringField("date", dateElement.text(), Field.Store.YES));
         }
+        return luceneDoc;
+    }
 
+    // Method to parse a single LATIMES file and return a Lucene document
+    public static Document parseLADocument(Element docElement) {
+        Document luceneDoc = new Document();
+
+        Element docnoElement = docElement.selectFirst("DOCNO");
+        if (docnoElement != null) {
+            luceneDoc.add(new StringField("docno", docnoElement.text(), Field.Store.YES));
+        }
+
+        Element docidElement = docElement.selectFirst("DOCID");
+        if (docidElement != null) {
+            luceneDoc.add(new StringField("docid", docidElement.text(), Field.Store.YES));
+        }
+
+        Element headerElement = docElement.selectFirst("HEADLINE");
+        if (headerElement != null) {
+            luceneDoc.add(new TextField("HEADLINE", headerElement.text(), Field.Store.YES));
+        }
+
+        Element textElement = docElement.selectFirst("TEXT");
+        if (textElement != null) {
+            luceneDoc.add(new TextField("text", textElement.text(), Field.Store.YES));
+        }
+
+        Element dateElement = docElement.selectFirst("DATE");
+        if (dateElement != null) {
+            luceneDoc.add(new StringField("date", dateElement.text(), Field.Store.YES));
+        }
         return luceneDoc;
     }
 
     // Method to process all files in the FBIS folder
-    public static int processFBISFolder(IndexWriter writer, String folderPath) throws IOException {
+    public static int processFbisLatimesFolder(IndexWriter writer, String folderPath,  boolean isFBIS) throws IOException {
         File folder = new File(folderPath);
         File[] files = folder.listFiles();
         int documentCount = 0; // Counter to track the number of indexed documents
 
         if (files != null) {
-//            int fileCount = 0; // Add a counter to limit to 5 files
             for (File file : files) {
-//                if (fileCount >= 1) {
-//                    break; // Stop after processing 5 files
-//                }
                 System.out.println("Processing file: " + file.getName());
                 String content = new String(Files.readAllBytes(Paths.get(file.getAbsolutePath())), "UTF-8");
                 org.jsoup.nodes.Document jsoupDoc = Jsoup.parse(content, "", org.jsoup.parser.Parser.xmlParser());
@@ -82,13 +108,12 @@ public class Main {
                 Elements docs = jsoupDoc.select("DOC");
 
                 for (Element docElement : docs) {
-                    Document luceneDoc = parseFBISDocument(docElement);
+                    Document luceneDoc = isFBIS ? parseFBISDocument(docElement) : parseLADocument(docElement);
                     if (luceneDoc != null) {
                         writer.addDocument(luceneDoc);
                         documentCount++;
                     }
                 }
-//                fileCount++; // Increment file count
             }
         }
         return documentCount; // Return the total number of indexed documents
@@ -101,17 +126,23 @@ public class Main {
             Directory indexDirectory = FSDirectory.open(Paths.get(INDEX_DIRECTORY));
             Analyzer analyzer = new EnglishAnalyzer();
             IndexWriterConfig config = new IndexWriterConfig(analyzer);
-            config.setSimilarity(new BM25Similarity()); // Use BM25 similarity
+            config.setSimilarity(new BM25Similarity()); // Can change the Similarity here
 
             IndexWriter writer = new IndexWriter(indexDirectory, config);
 
             // Process the FBIS folder and index all documents, keeping track of the count
-            int totalIndexedDocuments = processFBISFolder(writer, FILE_PATH_FBIS);
+            int totalIndexedDocumentsFbis = processFbisLatimesFolder(writer, FILE_PATH_FBIS, true);
+            // Process the LATIMES folder and index all documents
+            int totalIndexedDocumentsLatimes = processFbisLatimesFolder(writer, FILE_PATH_LATIMES, false);
 
-            // Commit and close the index
             writer.close();
+
+            // Print total documents indexed from both folders
             System.out.println("Indexing complete!");
-            System.out.println("Total documents indexed: " + totalIndexedDocuments);
+            System.out.println("Total FBIS documents indexed: " + totalIndexedDocumentsFbis);
+            System.out.println("Total LATIMES documents indexed: " + totalIndexedDocumentsLatimes);
+            System.out.println("Total documents indexed: " + (totalIndexedDocumentsFbis + totalIndexedDocumentsLatimes));
+
 
         } catch (IOException e) {
             e.printStackTrace();
